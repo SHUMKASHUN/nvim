6f2c980571fd9a532cd484f272b6de316460206b
SHUMKASHUN <ksshumab@connect.ust.hk>
2024-02-28 22:39:40 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- require "plugins.init"
+require "config.init"
+
+
+
+require("lazy").setup("plugins")
+-- require("catppuccin").setup({flavour="frappe"})
+-- vim.cmd.colorscheme "catppuccin"
+
+
+
+
